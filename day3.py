05dfddd0980e503873afc3c8d2d8e060616a8ab7
AlderSan <gentr023@gmail.com>
2026-01-20 20:25:46 -0600

    Complete day 3 part 2

--- day3.py
+++ day3.py
@@ -3,25 +3,34 @@ import re
 
 puzzle_input = get_data(day=3, year=2024)
 
-QUESTION_PART = 1
+QUESTION_PART = 2
 
-test_input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+test_input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 #X and Y are 1-3 digit numbers
 #mul(x,y) = X * Y
 #ignore every other character
 
-def input_into_list_of_instructions(puzzleinput: str) -> list[str]:
-    instructions_list = re.findall(r'mul\([0-9]{1,3},[0-9]{1,3}\)', puzzleinput)
+def input_into_list_of_instructions(puzzleinput: str) -> list[tuple[str]]:
+    if QUESTION_PART == 2:
+        instructions_list = re.findall(r"(mul\([0-9]{1,3},[0-9]{1,3}\))|(do\(\))|(don't\(\))", puzzleinput)
+    else:
+        instructions_list = re.findall(r'mul\([0-9]{1,3},[0-9]{1,3}\)', puzzleinput)
+    instructions_list = [(tuple(x for x in _ if x)) for _ in instructions_list]
     return instructions_list
 
-def process_instructions(instr_list: list[str]) -> int:
+def process_instructions(instr_list: list[tuple[str]]) -> int:
     result = 0
+    enabled=True
     for instr in instr_list:
-        nums = instr[4:-1]
-        print(nums)
-        num1, num2 = nums.split(",")
-        result += int(num1) * int(num2)
+        if instr[0] == "do()":
+            enabled=True
+        elif instr[0] == "don't()":
+            enabled=False
+        elif enabled:
+            nums = instr[0][4:-1]
+            num1, num2 = nums.split(",")
+            result += int(num1) * int(num2)
     return result
 
 inst = input_into_list_of_instructions(puzzle_input)
